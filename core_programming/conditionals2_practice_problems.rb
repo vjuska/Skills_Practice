0bea015a7a4d29003af5fbb95eff4ef46366bda6
Vic <vjuska24@gmail.com>
2021-11-22 17:19:01 -0600

    added random comments

--- core_programming/conditionals2_practice_problems.rb
+++ core_programming/conditionals2_practice_problems.rb
@@ -10,6 +10,7 @@ end
 
 # 2. Use a variable to store a number, then write a condition that prints -1 if the number is less than 10, prints 1 if the number is greater than 10, and prints 0 if the number is equal to 10.
 
+#test
 # 3. Use variables to store two numbers, then write a condition that prints 1 if the numbers are both less than 10, and prints 0 otherwise.
 
 # 4. Use a variable to store a number, then write a condition that prints 1 if the number is over 9000, and prints -1 otherwise.
